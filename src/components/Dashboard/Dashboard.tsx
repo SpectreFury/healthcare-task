0e7ad063400ccd2a5d60d6ade6c572ec6fccc481
Ayush Soni <soni.ayush.2212@gmail.com>
2025-05-23 23:39:53 +0530

    Add dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,21 @@
+import { HiMagnifyingGlass } from "react-icons/hi2";
 import styles from "./Dashboard.module.css";
 import HealthDashboard from "./HealthDashboard/HealthDashboard";
+import { IoMdNotifications } from "react-icons/io";
 
 const Dashboard = () => {
   return (
     <section className={styles.dashboard}>
       <div className={styles.dashboardContainer}>
-        <input placeholder="Search" className={styles.input} />
+        <div className={styles.inputWrapper}>
+          <span className={styles.searchIcon}>
+            <HiMagnifyingGlass />
+          </span>
+          <input placeholder="Search" className={styles.input} />
+          <span className={styles.magnifyingGlassIcon}>
+            <IoMdNotifications />
+          </span>
+        </div>
         <HealthDashboard />
       </div>
     </section>
--- src/components/Dashboard/HealthDashboard/HealthDashboard.tsx
+++ src/components/Dashboard/HealthDashboard/HealthDashboard.tsx
@@ -1,5 +1,32 @@
+import styles from "./HealthDashboard.module.css";
+
+import { IoChevronDown } from "react-icons/io5";
+
 const HealthDashboard = () => {
-  return <div>Health</div>;
+  return (
+    <div className={styles.healthDashboard}>
+      <div className={styles.healthDashboardContainer}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Dashboard</h1>
+          <div className={styles.dropdown}>
+            This week
+            <IoChevronDown />
+          </div>
+        </div>
+        <div className={styles.dashboardItems}>
+          <div className={styles.cardHuman}></div>
+          <div className={styles.cardOrganContainer}>
+            <div className={styles.cardOrgan}></div>
+            <div className={styles.cardOrgan}></div>
+            <div className={styles.cardOrgan}></div>
+          </div>
+        </div>
+        <div>
+          Activity
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HealthDashboard;
